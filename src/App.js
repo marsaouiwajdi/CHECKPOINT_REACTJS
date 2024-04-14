a0bff3dc8a4e7488469736ee5c018c0dba94d891
marsaouiwajdi <wajdi.marsaoui1@gmail.com>
2024-04-14 13:47:24 +0200

    first  commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import Cards from "./component/Cards";
+import Card from "./component/Card";
+function App(){
+
+
+    return(
+        <div className="App">
+            <Navbar></Navbar>
+            <Cards></Cards>
+            
+        </div>
+    )
+}
+export default App;
---
+++ src/component/Cards.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+
+function Cards () {
+                
+    return (
+        <div>
+            <p class="h1"> This is our first checkpoint :) </p>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+        
+    )
+    
+}
+
+
+
+export default Cards;
